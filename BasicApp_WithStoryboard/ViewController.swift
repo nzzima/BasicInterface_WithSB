6d7351a86c91ab70cb1a5fad92570f9294a05e72
nzzima <nikita-krylov-2012@mail.ru>
2024-07-29 12:02:02 +0300

    Start storyboard

---
+++ BasicApp_WithStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicApp_WithStoryboard
+//
+//  Created by Nikita Krylov on 29.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
